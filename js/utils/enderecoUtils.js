1f989911b0d14193f0729ebd317860207925b2d2
Renan Amaral <renanalvarado123@gmail.com>
2026-06-15 11:21:22 -0300

    Fix: Validação de CEP finalizada

--- js/utils/enderecoUtils.js
+++ js/utils/enderecoUtils.js
@@ -4,6 +4,7 @@
 
 import { buscarCep } from "../services/api.js";
 import { aplicarMascaraCEP } from "./mascaras.js";
+import { mostrarErro, limparErros } from "./formUtils.js";
 
 // ======================================
 // CONFIGURAÇÃO DA BUSCA DE CEP
@@ -34,14 +35,30 @@ export function configurarBuscaCEP(modoEdicao = false) {
 
     if (cep.length !== 8) return;
 
+    if (cep.length < 8) {
+      limparEndereco();
+
+      return;
+    }
+
     try {
+      limparErros();
+
       const endereco = await buscarCep(cep);
+
+      if (endereco.cep === null) {
+        throw new Error("CEP não encontrado");
+      }
 
       preencherEndereco(endereco);
 
       liberarCamposEndereco();
     } catch (erro) {
-      console.error("Erro ao buscar CEP:", erro);
+      limparEndereco();
+
+      bloquearCamposEndereco();
+
+      mostrarErro(cepInput, "CEP inválido ou não encontrado.");
     }
   });
 }
